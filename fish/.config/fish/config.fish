9d1e0f77ec688d4f3859869acc0cf13ef89326bb
Anderson Konzen <anderson.konzen@gmail.com>
2017-05-07 23:58:59 -0300

    Add option to load local.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,12 @@
 # Environment variables
 source $HOME/.config/fish/env.fish
 
-if status --is-interactive
-    eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+# Load preferred theme for shell
+if test -f $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+  eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+end
+
+# Load local config, ignored by git
+if test -f $HOME/.config/fish/local.fish
+  . $HOME/.config/fish/local.fish
 end
