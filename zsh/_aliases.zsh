9c87f466b0fb8aacc39b78e861179f2cf056643e
Anderson Konzen <anderson@anderkonzen.com>
2023-05-10 19:48:27 -0300

    Add ls aliases

--- zsh/_aliases.zsh
+++ zsh/_aliases.zsh
@@ -2,3 +2,9 @@ alias df='df -P -kHl' # disk free report
 alias du='du -h -c' # calculate disk usage for a folder
 
 alias cat=bat
+
+# List directory contents
+alias lsa='ls -lah --color=auto'
+alias l='ls -lah --color=auto'
+alias ll='ls -lh --color=auto'
+alias la='ls -lAh --color=auto'
