e23759a9fc90f82c533828738ba758151edf8cd0
Anderson Konzen <anderson@anderkonzen.com>
2024-01-03 21:30:53 -0300

    wip nvim typecraft

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,30 @@
-require("anderkonzen")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "lua", "javascript", "elixir" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
--- nvim/.config/nvim/lua/anderkonzen/init.lua
+++
@@ -1,4 +0,0 @@
-require("anderkonzen.packer")
-require("anderkonzen.set")
-require("anderkonzen.remap")
-
