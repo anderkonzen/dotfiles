dc134498514792f93f6b0dfa46341410a4b0a8ff
Anderson Konzen <anderson@anderkonzen.com>
2024-02-19 23:40:15 -0300

    Add more plugins

---
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+    keys = {
+      { "<Leader>gg", "<Cmd>:LazyGit<CR>", desc = "Open LazyGit" }
+    }
+	},
+}
+
--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,14 +1,5 @@
-local nnoremap = require("user.keymaps-utils").nnoremap
-local vnoremap = require("user.keymaps-utils").vnoremap
-local inoremap = require("user.keymaps-utils").inoremap
-local tnoremap = require("user.keymaps-utils").tnoremap
-local xnoremap = require("user.keymaps-utils").xnoremap
-
 -- Disable Space bar since it'll be used as the Leader key
-nnoremap("<Space>", "<NOP>")
-
--- Toggles indent lines
--- vim.keymap.set("n", "<Leader>it", ":IndentBlanklineToggle<CR>")
+vim.keymap.set("n", "<Space>", "<NOP>", { noremap = true})
 
 -- Strip trailing whitespace
 -- vim.keymap.set("n", "<Leader>ss", ":StripWhitespace<CR>")
@@ -16,55 +7,60 @@ nnoremap("<Space>", "<NOP>")
 -- Toggles gitsigns
 -- vim.keymap.set("n", "<Leader>gst", ":Gitsigns toggle_signs<CR>")
 
--- Save with Leader key
-nnoremap("<Leader>w", vim.cmd.update, { silent = false })
+-- Save with Leader key or <C-s>
+vim.keymap.set("n", "<Leader>w", vim.cmd.update)
+vim.keymap.set("n", "<C-s>", vim.cmd.update)
 
 -- Toggle show tabs and trailing spaces
-nnoremap("<Leader>tt", ":set nolist!<CR>", { desc = "Show/hide tabs and trailing spaces" })
+vim.keymap.set("n", "<Leader>tt", ":set nolist!<CR>", { desc = "Toggle tabs and trailing spaces on or off" })
 
 -- Clear search with <esc>
 vim.keymap.set({ "i", "n" }, "<Esc>", "<Cmd>noh<CR><Esc>", { desc = "Escape and clear search highlights" })
 
 -- Yank from cursor to end of line
-nnoremap("Y", "y$", { desc = "Yank from cursor to end of line" })
+vim.keymap.set("n", "Y", "y$", { desc = "Yank from cursor to end of line" })
 
 -- Start a replace command using word under prompt
-nnoremap("<Leader>s", ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<Leader>s", ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>")
 
 -- Enforce purity
-nnoremap("<Left>", "<NOP>")
-nnoremap("<Right>", "<NOP>")
-nnoremap("<Up>", "<NOP>")
-nnoremap("<Down>", "<NOP>")
+vim.keymap.set("n", "<Left>", "<NOP>", { noremap = true })
+vim.keymap.set("n", "<Right>", "<NOP>", { noremap = true })
+vim.keymap.set("n", "<Up>", "<NOP>", { noremap = true })
+vim.keymap.set("n", "<Down>", "<NOP>", { noremap = true })
 
 -- Better up/down
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Move to window using the <ctrl> hjkl keys
-nnoremap("<C-h>", "<C-w>h", { desc = "Go to left window"})
-nnoremap("<C-j>", "<C-w>j", { desc = "Go to lower window"})
-nnoremap("<C-k>", "<C-w>k", { desc = "Go to upper window"})
-nnoremap("<C-l>", "<C-w>l", { desc = "Go to right window"})
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window"})
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window"})
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window"})
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window"})
 
-inoremap("<C-c>", "<Esc>")
+-- <C-c> also exits insert mode
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
-nnoremap("Q", "<NOP>")
+-- I'm too fast 
+vim.keymap.set("n", "Q", "<NOP>")
 
 -- <Tab> / <s-Tab> | Circular windows navigation
-nnoremap("<Tab>", "<C-w>w")
-nnoremap("<S-Tab>", "<S-w>W")
+vim.keymap.set("n", "<Tab>", "<C-w>w")
+vim.keymap.set("n", "<S-Tab>", "<S-w>W")
 
 -- Quickly move between current and last buffers
-nnoremap("<Leader><Leader>", ":e#<CR>")
+vim.keymap.set("n", "<Leader><Leader>", ":e#<CR>")
 
 -- Move Lines
-nnoremap("<S-Down>", "<cmd>m .+1<CR>==", { desc = "Move down" })
-nnoremap("<S-Up>", "<cmd>m .-2<CR>==", { desc = "Move up" })
-inoremap("<S-Down>", "<esc><cmd>m .+1<CR>==gi", { desc = "Move down" })
-inoremap("<S-Up>", "<esc><cmd>m .-2<CR>==gi", { desc = "Move up" })
-vnoremap("<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move down" })
-vnoremap("<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move up" })
+vim.keymap.set("n", "<S-Down>", "<Cmd>m .+1<CR>==", { desc = "Move down" })
+vim.keymap.set("n", "<S-Up>", "<Cmd>m .-2<CR>==", { desc = "Move up" })
+vim.keymap.set("i", "<S-Down>", "<Esc><cmd>m .+1<CR>==gi", { desc = "Move down" })
+vim.keymap.set("i", "<S-Up>", "<Esc><cmd>m .-2<CR>==gi", { desc = "Move up" })
+vim.keymap.set("v", "<S-Down>", ":m '>+1<CR>gv=gv", { desc = "Move down" })
+vim.keymap.set("v", "<S-Up>", ":m '<-2<CR>gv=gv", { desc = "Move up" })
 
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true})
+vim.keymap.set("v", ">", ">gv", { noremap = true})
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true})
+vim.keymap.set("v", "<", "<gv", { noremap = true})
