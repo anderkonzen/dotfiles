b0bed4e29dd99622e940d032e46fdcf089dea13a
Anderson Konzen <anderson@anderkonzen.com>
2024-05-27 10:46:22 +0200

    Remove current nvim config

--- nvim/.config/nvim/lua/user/globals.lua
+++
@@ -1,9 +0,0 @@
--- Set <Space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader might
---  be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Disable Perl provider (see `:checkhealth`)
-vim.g.loaded_perl_provider = 0
