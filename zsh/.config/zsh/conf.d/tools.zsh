3744d5db040d96bde2fc230738c0e12c2a154398
Anderson Konzen <anderson@anderkonzen.com>
2026-04-11 15:41:39 -0300

    Pi suggestions (#12)

--- zsh/.config/zsh/conf.d/tools.zsh
+++ zsh/.config/zsh/conf.d/tools.zsh
@@ -1,5 +1,14 @@
 # tools.zsh — third-party tool integrations
 
+# AWS
+export AWS_CLI_AUTO_PROMPT=on-partial
+
+# bat
+export BAT_THEME="Catppuccin Mocha"
+
+# eza
+export EZA_CONFIG_DIR="$HOME/.config/eza"
+
 # zoxide — smarter cd
 eval "$(zoxide init zsh)"
 
@@ -15,4 +24,4 @@ eval "$(atuin init zsh --disable-up-arrow)"
 eval "$(direnv hook zsh)"
 
 # exercism completions
-source <(exercism completion zsh)
+command -v exercism >/dev/null && source <(exercism completion zsh)
--- zsh/.zprofile
+++ zsh/.zprofile
@@ -1,7 +1,1 @@
 # .zprofile — loaded for login shells only
-
-# History
-export HISTFILE="$ZDOTDIR/.zsh_history"
-export HISTSIZE=5000000
-export SAVEHIST=5000000
-export LISTMAX=1000
